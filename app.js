97f478bba3c0af64bef33e4ed6512df915e46bbb
Micah Monfrini <ffrock905@gmail.com>
2018-11-29 16:29:53 -0600

    create event listener function

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
+// UI variables
 const form = document.querySelector('#task-form')
 const taskList = document.querySelector('.collection')
 const clearBtn = document.querySelector('.clear-tasks')
 const filter = document.querySelector('#input-field')
 const taskInput = document.querySelector('#task')
+
+//Load all event listeners
+loadEventListeners()
+
+//Event listeners function
+function loadEventListeners() {
+  //Add task
+  form.addEventListener('submit', addTask)
+}
+
+//Add task
+
+function addTask() {
+  
+}
