4d0239c056e4bfa1d761f042020589e22da76fd6
Micah Monfrini <ffrock905@gmail.com>
2018-12-04 16:30:34 -0600

    store task function complete

--- app.js
+++ app.js
@@ -117,5 +117,11 @@ function storeTask(task) {
   let tasks;
   if (localStorage.getItem('tasks') === null) {
     tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
   }
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
